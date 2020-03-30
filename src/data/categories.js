c3a519b640da3f643cf00e08b32f7ab7d51ef04d
danielkjellid <daniel.kjellid@gmail.com>
2020-03-30 15:44:25 +0200

    Started adding markuup and logic to explore.

---
+++ src/data/categories.js
@@ -0,0 +1,14 @@
+export const categories = [
+    {
+        id: 1,
+        name: 'Mat'
+    },
+    {
+        id: 2,
+        name: 'Barer'
+    },
+    {
+        id: 3,
+        name: 'Shopping'
+    },
+]
---
+++ src/data/places2.js
@@ -0,0 +1,82 @@
+export const places2 = [
+    {
+        id: 1,
+        boroughId: 3,
+        name: 'Frogner 1',
+        image: 'smalhans.jpg',
+        address: 'Bjerregaardsgate 2A, 0172 Oslo',
+        categoryId: 1,
+        website: 'Place website',
+        reviews: {
+            environment: 4,
+            menu: 5,
+            service: 4,
+            value: 5,
+            amountOfReviwes: 29
+        },
+        pricepoint: 'Lavt',
+        bio: 'Place bio',
+        types: ['Michelin', 'Fusion'],
+        tags: ['Uformelt', 'Uteservering', 'Veldedighet', 'Rullestolvennlig']
+    },
+    {
+        id: 2,
+        boroughId: 3,
+        name: 'Frogner 2',
+        image: 'smalhans.jpg',
+        address: 'Bjerregaardsgate 2A, 0172 Oslo',
+        categoryId: 2,
+        website: 'Place website',
+        reviews: {
+            environment: 3,
+            menu: 3,
+            service: 3,
+            value: 3,
+            amountOfReviwes: 29
+        },
+        pricepoint: 'Lavt',
+        bio: 'Place bio',
+        types: ['Michelin', 'Fusion', 'Test'],
+        tags: ['Uformelt', 'Uteservering', 'Veldedighet', 'Rullestolvennlig']
+    },
+    {
+        id: 3,
+        boroughId: 3,
+        name: 'Frogner 3',
+        image: 'smalhans.jpg',
+        address: 'Bjerregaardsgate 2A, 0172 Oslo',
+        categoryId: 3,
+        website: 'Place website',
+        reviews: {
+            environment: 4,
+            menu: 5,
+            service: 4,
+            value: 5,
+            amountOfReviwes: 29
+        },
+        pricepoint: 'Lavt',
+        bio: 'Place bio',
+        types: ['Michelin', 'Fusion'],
+        tags: ['Uformelt', 'Uteservering', 'Veldedighet', 'Rullestolvennlig']
+    },
+    {
+        id: 4,
+        boroughId: 3,
+        name: 'Frogner 4',
+        image: 'smalhans.jpg',
+        address: 'Bjerregaardsgate 2A, 0172 Oslo',
+        categoryId: 1,
+        website: 'Place website',
+        reviews: {
+            environment: 4,
+            menu: 5,
+            service: 4,
+            value: 5,
+            amountOfReviwes: 29
+        },
+        pricepoint: 'Lavt',
+        bio: 'Place bio',
+        types: ['Michelin', 'Fusion'],
+        tags: ['Uformelt', 'Uteservering', 'Veldedighet', 'Rullestolvennlig']
+    },
+]
