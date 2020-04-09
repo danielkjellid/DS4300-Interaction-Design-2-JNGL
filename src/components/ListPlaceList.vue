2a9b5aa94bdf7358ac3b68cf506143c044cea227
danielkjellid <daniel.kjellid@gmail.com>
2020-04-09 23:44:41 +0200

    Minor esthetical changes.

--- src/components/ListPlaceList.vue
+++ src/components/ListPlaceList.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- component to add place item inside a container list -->
-  <div class="bg-white rounded-lg shadow">
+  <div class="bg-white rounded shadow">
     <!-- loop through the palces array -->
     <div v-for="place in places" :key="place.id">
       <app-core-place-item :place="place" :favoriteBtnClick="favoriteBtnClick"></app-core-place-item>
